9653d9309d698309dc17ebb2efb2073ab525f1bb
Md. Kutub Uddin <kutub01753@gmail.com>
2026-04-14 13:28:48 +0600

    footer complited

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,25 +2,56 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#244D3F] text-white border-t mt-16">
-      
-      <div className=" ">
+    <footer className="bg-[#244D3F] text-white mt-16 mx-auto">
 
-     
-        <div>
-          <h1 className="text-3xl font-bold text-white text-center">
-            KeenKeeper
-          </h1>
-          <p className="text-sm text-gray-500 mt-2 text-center">
-            Stay connected with your friends and never lose touch again.
-          </p>
+
+      <div className=" max-w-xl mx-auto px-6 py-12 text-center justify-center ">
+        <img src="/public/assets/logo-xl.png" alt="" />
+
+        
+        <p className="text-sm mt-3 text-gray-200 max-w-md mx-auto">
+          Stay connected with your friends and never lose touch again.
+        </p>
+
+       
+        <p className="text-sm mt-6 text-gray-300">
+          Follow us
+        </p>
+
+        <div className="flex justify-center gap-4 mt-4">
+
+          <img
+            src="/assets/instagram.png"
+            alt="instagram"
+            className="w-6 h-6 cursor-pointer hover:scale-110 transition"
+          />
+
+          <img
+            src="/assets/facebook.png"
+            alt="facebook"
+            className="w-6 h-6 cursor-pointer hover:scale-110 transition"
+          />
+
+          <img
+            src="/assets/twitter.png"
+            alt="twitter"
+            className="w-6 h-6 cursor-pointer hover:scale-110 transition"
+          />
+
         </div>
 
       </div>
 
-      <div className="border-t py-4 text-center text-sm text-gray-500">
-        © {new Date().getFullYear()} KeenKeeper. All rights reserved.
+     
+      <div className=" flex justify-between border-t border-white/20 py-4 text-center text-sm text-gray-300">
+        <div>
+            © {new Date().getFullYear()} KeenKeeper. All rights reserved.
+        </div>
+        <div>
+        Privacy Policy           Terms of Service           Cookies
       </div>
+      </div>
+      
 
     </footer>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,8 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between p-4 shadow bg-white items-center ">
-      <h1 className="font-bold text-2xl text-[#244D3F]">Keen<span className="font-semibold ">Keeper</span></h1>
-
+    <div className=" mx-auto container flex justify-between p-4 shadow bg-white items-center ">
+      <img src="/public/assets/logo.png" alt="" />
       <div className="flex gap-6 items-center justify-center ">
         <NavLink to="/" className={({isActive}) => isActive ? "mt-4 px-4 py-2 bg-[#244D3F] text-white rounded" : ""}>
           Home
